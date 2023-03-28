94ab1d051452a474697b44300e7e09e9a875c228
Mostafa Vazini <mvazini@storkcraft.com>
2023-03-28 12:44:01 -0700

    set Azure Ad values

--- TestAzureKeyVault.Api/Endpoints/Post/PostMapGroup.cs
+++ TestAzureKeyVault.Api/Endpoints/Post/PostMapGroup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web.Resource;
 using TestAzureKeyVault.Shared.Contracts;
 
 namespace TestAzureKeyVault.Api.Endpoints.Post;
@@ -7,13 +8,13 @@ public static class PostMapGroup
 {
     public static RouteGroupBuilder MapPosts(this RouteGroupBuilder group)
     {
-        group.MapGet("/", async (IPostRepository repository) =>
+        group.MapGet("/", [Authorize(Roles = "Manager"), RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")] async (IPostRepository repository) =>
         {
             var posts = await repository.GetAll();
             return posts;
         });
 
-        group.MapGet("/{id:int}", async (int id , IPostRepository repository) =>
+        group.MapGet("/{id:int}", async (int id, IPostRepository repository) =>
         {
             var posts = await repository.Get(id);
             return posts;
--- TestAzureKeyVault.Api/Program.cs
+++ TestAzureKeyVault.Api/Program.cs
@@ -1,5 +1,8 @@
 using Azure.Extensions.AspNetCore.Configuration.Secrets;
 using Azure.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 using TestAzureKeyVault.Api.Endpoints;
 using TestAzureKeyVault.Data;
 
@@ -8,20 +11,49 @@ var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
 builder.Configuration.AddAzureKeyVault(
-    new Uri(config["KeyVaultConfiguration:KeyVaultURL"]!),
-    new DefaultAzureCredential(
-        //new DefaultAzureCredentialOptions
-        //{
-        //    ManagedIdentityClientId = config["KeyVaultConfiguration:ClientId"]
-        //}
-        )
-    , new KeyVaultSecretManager()
+    new Uri(config["KeyVault:URL"]!),
+    new DefaultAzureCredential(),
+    new KeyVaultSecretManager()
     );
 
-// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(config);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger Azure AD Demo", Version = "v1" });
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Oauth2.0 wich uses AuthorizationCode flow",
+        Name = "oauth2.0",
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri(config["SwaggerAzureAd:AuthorizationUrl"]!),
+                TokenUrl = new Uri(config["SwaggerAzureAd:TokenUrl"]!),
+                Scopes = new Dictionary<string, string>
+                {
+                    { config["SwaggerAzureAd:Scope"]!,"Access API as User"}
+                }
+            }
+        }
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+            },
+            new[]{ config["SwaggerAzureAd:Scope"] }
+        }
+    });
+});
 builder.Services.DataServicesRegistration(builder.Configuration);
 
 var app = builder.Build();
@@ -32,10 +64,17 @@ app.RegisterEndpoints();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.OAuthClientId(config["SwaggerAzureAd:ClientId"]);
+        c.OAuthUsePkce();
+        c.OAuthScopeSeparator(" ");
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
